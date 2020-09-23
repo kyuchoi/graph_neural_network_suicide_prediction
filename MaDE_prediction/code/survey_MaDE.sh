2b85f2e8b4f3d5e1960791eefe177d776d0fe8f4
Kyu Sung Choi <kyuchoi@kaist.ac.kr>
2020-09-23 12:35:45 +0900

    Update survey_MaDE.sh

--- MaDE_prediction/code/survey_MaDE.sh
+++ MaDE_prediction/code/survey_MaDE.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-## NEVER give a space between file names when you enter the input files !!
-
 python data_read.py --new_subsample # NEED it to obtain edge matrix using qgraph, when you process just a single case
 Rscript EBICglasso_qgraph_MaDE.R
 #python main_made.py --num_epochs 5 # uncomment this ONLY, when you predict just a single case
